fc97f585757fd0640ba55c7d0728d38b3a1f46fe
Udara Rathnasooriya <bika.learning@gmail.com>
2024-03-07 17:22:42 +0530

    Add Mobile Menu

--- src/components/HorizontalNavBar.jsx
+++ src/components/HorizontalNavBar.jsx
@@ -1,15 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { main_logo, icon } from "../assets/index";
 import { navLinks } from "../constants";
-
-
+import { TfiMenu } from "react-icons/tfi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const HorizontalNavBar = () => {
+  const [toggle, setToggle] = useState(false);
+  console.log(toggle);
+  const handleToggleMenu = () => {
+    setToggle((prevValue) => !prevValue);
+  };
   return (
     <Fragment>
       <nav className=" bg-primary py-6 px-5 ss:px-10 sm:px-[60px] md:px-20   flex items-center justify-between">
         <img src={main_logo} className="w-[240px] h-9" />
-        <img src={icon} className="w-8 h-6  ss:hidden mt-3" />
+        {toggle ? (
+          <AiOutlineClose
+            className="w-8 h-8 text-white ss:hidden"
+            onClick={handleToggleMenu}
+          />
+        ) : (
+          <TfiMenu
+            className="w-8 h-8 text-white ss:hidden"
+            onClick={handleToggleMenu}
+          />
+        )}
         <ul className=" space-x-7 ss:flex hidden  text-white font-inter">
           {navLinks.map((link) => {
             return (
@@ -20,6 +35,22 @@ const HorizontalNavBar = () => {
           })}
         </ul>
       </nav>
+      {toggle && (
+        <nav className="bg-primary h-screen ss:hidden flex pt-8 border-t-2 justify-center">
+          <ul className=" space-y-5 flex flex-col  w-full mx-2  ss:hidden  text-white font-inter">
+            {navLinks.map((link) => {
+              return (
+                <li
+                  key={link.id}
+                  className="font-medium  rounded-md  text-gray-100 hover:bg-gray-300 hover:text-gray-800 hover:font-bold"
+                >
+                  <a href={`${link.title} `} className="block px-2 py-2">{link.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      )}
     </Fragment>
   );
 };
